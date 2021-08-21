be85e142d0e0991b4f1aadeefc36430a90b0cb16
Davi <davigurgel@virtual.ufc.br>
2021-08-21 11:39:39 -0300

    Adição de 2 novos métodos
    Retorna:
    Função soma
    13
    Método forEach
    [ 2, 4, 6, 8, 10 ]
    Método filter
    [ 2, 4, 6, 8, 10 ]

--- Revisão/revisao.js
+++ Revisão/revisao.js
@@ -1,5 +1,22 @@
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
 function soma (num1 , num2 ) {
     console.log(num1 + num2)
 }
-
-soma(5, 6)
+console.log("Função soma")
+soma(6, 7)
+function numerosPares(array){
+    let _numeros = []
+    array.forEach(element => {
+        if (element%2==0)
+            _numeros.push(element);
+    });
+    return _numeros
+}
+function numerosPares2(array){
+    return array.filter(num => num%2==0)
+}
+console.log("Método forEach")
+console.log(numerosPares(numeros))
+console.log("Método filter")
+console.log(numerosPares2(numeros))
